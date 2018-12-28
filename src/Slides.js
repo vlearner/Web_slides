a767c78bcbae6dab73e22e659044bf9b72fab688
vlearner <barotvishal12@outlook.com>
2018-12-28 07:16:30 -0600

    Web Presentation Slides

---
+++ src/Slides.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Slides extends React.Component {
+    render(){
+        return(
+            <div className="Slide">
+                <p>Slide</p>
+            </div>
+        )
+    }
+}
+
+
+export default Slides;
